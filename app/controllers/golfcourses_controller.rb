14bd44b9722a4248bd77991a4a27c32541c2b77b
EC2 Default User <ec2-user@ip-172-31-37-102.ap-northeast-1.compute.internal>
2023-04-06 04:02:37 +0000

    ゴルフコースの投稿、一覧、詳細ページ作成

--- app/controllers/golfcourses_controller.rb
+++ app/controllers/golfcourses_controller.rb
@@ -1,14 +1,31 @@
 class GolfcoursesController < ApplicationController
   def new
-    @golfcourse = Golfcourses.new
+    @golfcourse = Golfcourse.new
+  end
+
+  def create
+    # データを受け取り新規登録するためのインスタンス作成
+    golfcourse = Golfcourse.new(golfcourse_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    golfcourse.save
+    # トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
+    @golfcourses = Golfcourse.all
   end
 
   def show
+    @golfcourse = Golfcourse.find(params[:id])
   end
 
   def edit
+  end
+
+   private
+  # ストロングパラメータ
+  def golfcourse_params
+    params.require(:golfcourse).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get '/top' => 'homes#top'
   get 'golfcourses/new'
-  get 'golfcourses/index'
-  get 'golfcourses/show'
+  post 'golfcourses' => 'golfcourses#create'
+  get 'golfcourses' => 'golfcourses#index'
+  get 'golfcourses/:id' => 'golfcourses#show'
   get 'golfcourses/edit'
-  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
