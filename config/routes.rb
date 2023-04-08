4e05d72c750cb10f1b71721fabf4383672584e36
EC2 Default User <ec2-user@ip-172-31-37-102.ap-northeast-1.compute.internal>
2023-04-08 05:48:50 +0000

    usersコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   root to: 'homes#top'
   devise_for :users
-
   resources :golfcourses
-
   get "homes/about" => "homes#about", as: "about"
 end
