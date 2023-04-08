361927de7f86d1b50efd5c4ba973f4754149fc00
EC2 Default User <ec2-user@ip-172-31-37-102.ap-northeast-1.compute.internal>
2023-04-08 05:54:09 +0000

    routesの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   root to: 'homes#top'
   devise_for :users
-  resources :golfcourses
+  resources :golfcourses, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit]
   get "homes/about" => "homes#about", as: "about"
 end
