d257be46b369d77d9dad10c33fb0633fa0cefa7d
EC2 Default User <ec2-user@ip-172-31-37-102.ap-northeast-1.compute.internal>
2023-04-15 06:44:34 +0000

    コメント機能のバリデーション追加、コメントしたユーザーにリンク追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,11 +1,14 @@
 class PostCommentsController < ApplicationController
 
   def create
-    golfcourse = Golfcourse.find(params[:golfcourse_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.golfcourse_id = golfcourse.id
-    comment.save
-    redirect_to golfcourse_path(golfcourse)
+    @golfcourse = Golfcourse.find(params[:golfcourse_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.golfcourse_id = @golfcourse.id
+    if @post_comment.save
+      redirect_to golfcourse_path(@golfcourse)
+    else
+      render 'golfcourses/show'
+    end
   end
 
    def destroy
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -3,4 +3,6 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :golfcourse
 
+  validates :comment, presence: true
+
 end
